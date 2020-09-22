4710394fdd521f0bb76b6eee5af937adc98f6720
Dmitriy <mail@d-belyaev.ru>
2020-09-22 23:52:30 +0300

    rename makeGuard -> makeTypeofGuard helper

--- src/type-guards/typeGuards.ts
+++ src/type-guards/typeGuards.ts
@@ -9,17 +9,17 @@ type TypesMap = {
     'function': (...args: unknown[]) => unknown;
 };
 
-function makeGuard<T extends keyof TypesMap>(type: T): (v: unknown) => v is TypesMap[T] {
+function makeTypeofGuard<T extends keyof TypesMap>(type: T): (v: unknown) => v is TypesMap[T] {
     return (v): v is TypesMap[T] => typeof v === type;
 }
 
-export const isUndefined = makeGuard('undefined');
+export const isUndefined = makeTypeofGuard('undefined');
 export const isVoid = isUndefined as (v: unknown) => v is void;
-export const isBoolean = makeGuard('boolean');
-export const isString = makeGuard('string');
-export const isNumber = makeGuard('number');
-export const isBigInt = makeGuard('bigint');
-export const isSymbol = makeGuard('symbol');
-export const isObject = makeGuard('object');
-export const isFunction = makeGuard('function');
+export const isBoolean = makeTypeofGuard('boolean');
+export const isString = makeTypeofGuard('string');
+export const isNumber = makeTypeofGuard('number');
+export const isBigInt = makeTypeofGuard('bigint');
+export const isSymbol = makeTypeofGuard('symbol');
+export const isObject = makeTypeofGuard('object');
+export const isFunction = makeTypeofGuard('function');
 export const isNull = (v: unknown): v is null => v === null;
