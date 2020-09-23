5c7d4732ba4bf9e52f043493c52239c667f26a2b
Dmitriy <mail@d-belyaev.ru>
2020-09-24 00:10:37 +0300

    cell: rebuild fp-style functions

--- src/cell/fp.ts
+++ src/cell/fp.ts
@@ -1,33 +1,8 @@
-import {isFunction} from '@lambda-fn/type-guards';
-import {makeTag, InferTag} from '../_util';
+import type {Cell} from './cell';
 
-const tagger = makeTag('Cell');
-
-export type Cell<T> = {
-    readonly $: InferTag<typeof tagger>;
-    (): T;
-    (v: T): void;
-};
-
-export function makeCell<T>(initialValue: T): Cell<T> {
-    let value = initialValue;
-    return tagger<(v?: T) => T>((...args) => (args.length !== 0
-        ? (value = args[0]!)
-        : value
-    ));
-}
-
-export function isCell(maybeCell: unknown): maybeCell is Cell<unknown> {
-    return isFunction(maybeCell) && tagger.$(maybeCell);
-}
-
-export function isCellWith<T>(guard: (v: unknown) => v is T): (maybeCell: unknown) => maybeCell is Cell<T> {
-    return (maybeCell): maybeCell is Cell<T> => isCell(maybeCell) && guard(maybeCell());
-}
-
-export const get = <T>(cell: Cell<T>) => cell();
-export const set = <T>(cell: Cell<T>, value: T) => cell(value);
-export const update = <T>(cell: Cell<T>, updater: (value: T) => T) => cell(updater(cell()));
-export const clone = <T>(cell: Cell<T>) => makeCell(cell());
-export const map = <T, U>(mapper: (value: T) => U) => (cell: Cell<T>) => makeCell(mapper(cell()));
-export const fold = <T, U>(fld: (value: T) => U) => (cell: Cell<T>) => fld(cell());
+export const get = <T>(cell: Cell<T>) => cell.get();
+export const set = <T>(cell: Cell<T>, value: T) => cell.set(value);
+export const update = <T>(cell: Cell<T>, updater: (value: T) => T) => cell.update(updater);
+export const clone = <T>(cell: Cell<T>) => cell.clone();
+export const map = <T, U>(mapper: (value: T) => U) => (cell: Cell<T>) => cell.map(mapper);
+export const fold = <T, U>(mapper: (value: T) => U) => (cell: Cell<T>) => cell.fold(mapper);
