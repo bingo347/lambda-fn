ea7e8c8755a660e473de9019f37b5687f79ac469
Dmitriy <mail@d-belyaev.ru>
2020-09-23 23:19:17 +0300

    add new Cell realisation

---
+++ src/cell/cell.ts
@@ -0,0 +1,42 @@
+import type {TypeGuard} from '@lambda-fn/type-guards';
+
+type ValueFN<V, R> = (value: V) => R;
+
+export interface CellStatic {
+    <T>(initialValue: T): Cell<T>;
+    isCell(maybeCell: unknown): maybeCell is Cell<unknown>;
+    isCellWith<T>(guard: TypeGuard<T>, maybeCell: unknown): maybeCell is Cell<T>;
+}
+
+export interface Cell<T> {
+    get(): T;
+    set(value: T): void;
+    update(updater: (value: T) => T): void;
+    subscribe(subscription: (value: T) => void): () => void;
+    clone(): Cell<T>;
+    map<U>(mapper: (value: T) => U): Cell<U>;
+    fold<U>(mapper: (value: T) => U): U;
+}
+
+// eslint-disable-next-line max-lines-per-function
+export const Cell = <T>(initialValue: T): Cell<T> => {
+    const get = () => currentValue;
+    const set = (value: T) => {
+        if(value === currentValue) { return; }
+        currentValue = value;
+        for(const subscription of subscriptions.values()) {
+            subscription(currentValue);
+        }
+    };
+    const update = (updater: ValueFN<T, T>) => set(updater(currentValue));
+    const subscribe = (subscription: ValueFN<T, void>) => (
+        subscriptions.add(subscription),
+        () => void subscriptions.delete(subscription)
+    );
+    const clone = () => Cell(currentValue);
+    const map = <U>(mapper: ValueFN<T, U>) => Cell(mapper(currentValue));
+    const fold = <U>(mapper: ValueFN<T, U>) => mapper(currentValue);
+    const subscriptions = new Set<ValueFN<T, void>>();
+    let currentValue = initialValue;
+    return {get, set, update, subscribe, clone, map, fold};
+};
