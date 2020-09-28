fade0f7a83ea7f6ef653b5484690abcb7e4e7043
Dmitriy <mail@d-belyaev.ru>
2020-09-28 23:16:59 +0300

    cell: fix deep equality

---
+++ src/cell/cell.test.ts
@@ -0,0 +1,61 @@
+jest.mock('@lambda-fn/type-guards', () => require('../type-guards'), {virtual: true});
+
+import {Cell} from './cell';
+
+test('isCell', () => {
+    const guard = (v: unknown): v is 1 => v === 1;
+    const cell0 = Cell(0);
+    const cell1 = Cell(1);
+
+    expect(Cell.isCell(cell0)).toBe(true);
+    expect(Cell.isCell(cell1)).toBe(true);
+    expect(Cell.isCell({})).toBe(false);
+
+    expect(Cell.isCellWith(guard, cell0)).toBe(false);
+    expect(Cell.isCellWith(guard, cell1)).toBe(true);
+});
+
+test('cell.value as get/set', () => {
+    const cell = Cell(0);
+    expect(cell.value).toBe(0);
+
+    cell.set(1);
+    expect(cell.value).toBe(1);
+
+    cell.value = -1;
+    expect(cell.get()).toBe(-1);
+});
+
+// eslint-disable-next-line max-lines-per-function
+test('cell.subscribe', () => {
+    const fn = jest.fn<void, [number]>();
+    const cell = Cell(1);
+
+    const unsubscribe = cell.subscribe(fn);
+    expect(fn).toBeCalledTimes(1);
+    expect(fn).toBeCalledWith(1);
+
+    cell.set(2);
+    expect(fn).toBeCalledTimes(2);
+    expect(fn).toBeCalledWith(2);
+
+    cell.set(2);
+    expect(fn).toBeCalledTimes(2);
+
+    cell.update(v => v + 1);
+    expect(fn).toBeCalledTimes(3);
+    expect(fn).toBeCalledWith(3);
+
+    cell.value++;
+    expect(fn).toBeCalledTimes(4);
+    expect(fn).toBeCalledWith(4);
+
+    unsubscribe();
+    cell.set(0);
+    expect(fn).toBeCalledTimes(4);
+    expect(fn).toBeCalledWith(4);
+});
+
+test('similar cells are deep equal', () => {
+    expect(Cell({x: 0})).toStrictEqual(Cell({x: 0}));
+});
--- src/cell/cell.ts
+++ src/cell/cell.ts
@@ -1,8 +1,21 @@
 import {TypeGuard, isObject} from '@lambda-fn/type-guards';
 
 type ValueFN<V, R> = (value: V) => R;
+type ExtendFN = <T>(
+    get: Cell<T>['get'],
+    set: Cell<T>['set'],
+    subscribe: Cell<T>['subscribe']
+) => Partial<Cell<T>>;
+
 const enum CellKind { Cell }
 const GUARD = Symbol();
+const extenders: [ExtendFN, boolean][] = [];
+const makeDescriptor = (value: any, configurable = false, enumerable = false, writable = false): PropertyDescriptor => ({
+    value,
+    configurable,
+    enumerable,
+    writable
+});
 
 export interface CellStatic {
     <T>(initialValue: T): Cell<T>;
@@ -26,7 +39,9 @@ export const isCell = (maybeCell: unknown): maybeCell is Cell<unknown> => isObje
 export const isCellWith = <T>(guard: TypeGuard<T>, maybeCell: unknown): maybeCell is Cell<T> => isCell(maybeCell) && maybeCell.fold(guard);
 
 // eslint-disable-next-line max-lines-per-function
-export const Cell: CellStatic = Object.assign(<T>(initialValue: T): Cell<T> => {
+export const Cell: CellStatic = Object.defineProperties(<T>(initialValue: T): Cell<T> => {
+    let currentValue = initialValue;
+    const subscriptions = new Set<ValueFN<T, void>>();
     const get = () => currentValue;
     const set = (value: T) => {
         if(value === currentValue) { return; }
@@ -35,17 +50,41 @@ export const Cell: CellStatic = Object.assign(<T>(initialValue: T): Cell<T> => {
             subscription(currentValue);
         }
     };
-    const update = (updater: ValueFN<T, T>) => set(updater(currentValue));
-    const subscribe = (subscription: ValueFN<T, void>) => (
-        subscriptions.add(subscription),
-        subscription(currentValue),
-        () => void subscriptions.delete(subscription)
-    );
-    const clone = () => Cell(currentValue);
-    const map = <U>(mapper: ValueFN<T, U>) => Cell(mapper(currentValue));
-    const fold = <U>(mapper: ValueFN<T, U>) => mapper(currentValue);
-    const subscriptions = new Set<ValueFN<T, void>>();
-    let currentValue = initialValue;
-    const cell: Omit<Cell<T>, 'value'> = {get, set, update, subscribe, clone, map, fold, [GUARD]: CellKind.Cell};
-    return Object.defineProperty(cell, 'value', {get, set});
-}, {isCell, isCellWith});
+    return upgradeCellWithExtenders(Object.defineProperties({}, {
+        value: {get, set, enumerable: true},
+        get: makeDescriptor(get),
+        set: makeDescriptor(set),
+        subscribe: makeDescriptor((subscription: ValueFN<T, void>) => (
+            subscriptions.add(subscription),
+            subscription(currentValue),
+            () => void subscriptions.delete(subscription)
+        ))
+    }));
+}, {
+    isCell: makeDescriptor(isCell),
+    isCellWith: makeDescriptor(isCellWith)
+});
+
+export const extend = (cb: ExtendFN, configurable = true): void => void extenders.push([cb, configurable]);
+
+const upgradeCellWithExtenders = <T>(cell: Partial<Cell<T>>): Cell<T> => extenders.reduce((partialCell, [cb, configurable]) => (
+    mergeCell(partialCell, cb(partialCell.get!, partialCell.set!, partialCell.subscribe!), configurable)
+), cell) as any as Cell<T>;
+
+const mergeCell = <T>(currentCell: Partial<Cell<T>>, extendedCell: Partial<Cell<T>>, configurable: boolean) => {
+    for(const key of Object.getOwnPropertyNames(extendedCell) as (keyof Cell<T>)[]) {
+        Object.defineProperty(currentCell, key, makeDescriptor(extendedCell[key], configurable));
+    }
+    for(const key of Object.getOwnPropertySymbols(extendedCell) as (keyof Cell<T>)[]) {
+        Object.defineProperty(currentCell, key, makeDescriptor(extendedCell[key], configurable));
+    }
+    return currentCell;
+};
+
+extend((get, set) => ({
+    update: updater => set(updater(get())),
+    clone: () => Cell(get()),
+    map: mapper => Cell(mapper(get())),
+    fold: mapper => mapper(get()),
+    [GUARD]: CellKind.Cell
+}), false);
