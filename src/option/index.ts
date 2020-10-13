7fc8d1d5a5a79b03d0cf3548eeaeef39a4c0fef8
Dmitriy <mail@d-belyaev.ru>
2020-10-13 23:57:12 +0300

    option: upd index

--- src/option/index.ts
+++ src/option/index.ts
@@ -2,4 +2,6 @@ import {patch, checkPatchValue, isSomeKind, isNoneKind} from './internal';
 export const __internal = {patch, checkPatchValue, isSomeKind, isNoneKind};
 export * from './option';
 export * from './guards';
+export * from './fromNullable';
 export * from './unwrapping';
+export * from './transforming';
