c29b301ecfcd8a013183257295037f772b3b7e7c
Dmitriy <mail@d-belyaev.ru>
2020-10-23 00:01:36 +0300

    result: add internal helpers for impl methods

--- src/result/internal.ts
+++ src/result/internal.ts
@@ -1,9 +1,44 @@
-import type {Ok, Err} from './result';
+import type {Result, Ok, Err, ResultInstance} from './result';
+import {makeDescriptor} from '../_util';
+
+type AnyMethod<T, E> = ResultInstance<T, E>[keyof ResultInstance<T, E>];
+type Implementor = [
+    method: string,
+    whenOk: <T, E>(value: T) => AnyMethod<T, E>,
+    whenErr: <T, E>(error: E) => AnyMethod<T, E>,
+    configurable: boolean
+];
+const implementors: Array<Implementor> = [];
 
 // eslint-disable-next-line @typescript-eslint/no-shadow
 export const enum ResultKind { Ok, Err }
 export const GUARD = Symbol();
 export const VALUE = Symbol();
+export const impl = <M extends keyof ResultInstance<any, any>>(
+    method: M,
+    whenOk: <T, E>(value: T) => ResultInstance<T, E>[M],
+    whenErr: <T, E>(error: E) => ResultInstance<T, E>[M],
+    configurable = true
+): void => {
+    implementors.push([method, whenOk as Implementor[1], whenErr as Implementor[2], configurable]);
+};
 
-export declare function makeResult<T>(kind: ResultKind.Ok, value: T): Ok<T>;
-export declare function makeResult<E>(kind: ResultKind.Err, value: E): Err<E>;
+export function makeResult<T>(kind: ResultKind.Ok, value: T): Ok<T>;
+export function makeResult<E>(kind: ResultKind.Err, value: E): Err<E>;
+export function makeResult<T, E>(kind: ResultKind, value: T | E): Result<T, E> {
+    const protoResult = {} as Result<T, E>;
+    Object.defineProperty(protoResult, VALUE, makeDescriptor(value, false, true));
+    Object.defineProperty(protoResult, GUARD, makeDescriptor(kind, false, true));
+    Object.defineProperty(protoResult, Symbol.toStringTag, {get: () => 'Result'});
+    return implementors.reduce(mergeResultReducer<T, E>(kind, value), protoResult);
+}
+
+function mergeResultReducer<T, E>(kind: ResultKind, value: T | E) {
+    return (result: Result<T, E>, [method, whenOk, whenErr, configurable]: Implementor) => (
+        Object.defineProperty(result, method, makeDescriptor((kind === ResultKind.Ok
+            ? whenOk(value)
+            : whenErr(value)
+        ), configurable)),
+        result
+    );
+}
