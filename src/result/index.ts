31acf8f69431a1126a38be8a34c28af653f88e03
Dmitriy <mail@d-belyaev.ru>
2020-10-27 22:50:48 +0300

    result: upd index

--- src/result/index.ts
+++ src/result/index.ts
@@ -1,4 +1,11 @@
-export {Ok, Err, Result} from './types';
-export * from './builders';
+import {impl} from './internal';
+export const __internal = {impl};
+export {Result as default} from './result';
+export * from './result';
 export * from './guards';
-export * from './operators';
+export * from './tryCatch';
+export * from './unwrapping';
+export * from './transforming';
+export * from './flat';
+export * from './apply';
+export * from './legacy';
