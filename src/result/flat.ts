d2387e64c2bab7233a37f59ddc3b65da27724443
Dmitriy <mail@d-belyaev.ru>
2020-10-27 20:57:56 +0300

    result: impl flat & apply

---
+++ src/result/flat.ts
@@ -0,0 +1,10 @@
+import {Result, Err} from './result';
+import {impl} from './internal';
+import {isResult} from './guards';
+import {_assert} from '../_util';
+
+export const flat = <U, O, E>(result: Result<Result<U, O>, E>): Result<U, E | O> => result.flat();
+impl('flat', (innerResult => <U, O>() => {
+    _assert(isResult(innerResult), 'flat may called only on nested Result', TypeError);
+    return innerResult as Result<U, O>;
+}), (error => () => Err(error)), false);
