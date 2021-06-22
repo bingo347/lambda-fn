79dc6832bb70660ad87d763e082c827a9dd19db8
Dmitry Belyaev <mail@d-belyaev.ru>
2021-06-22 20:51:57 +0300

    chore: upd build

--- packages/result/src/apply.ts
+++ packages/result/src/apply.ts
@@ -3,6 +3,7 @@ import {Result, Err} from './result';
 import {impl} from './internal';
 import {_assert} from './_util';
 
+// @ts-expect-error: TODO:
 impl('apply', (mapper => <U, R, O>(target: Result<U, O>) => {
     _assert(isFunction(mapper), 'apply may called only on Result with function', TypeError);
     return target.map(mapper) ;
