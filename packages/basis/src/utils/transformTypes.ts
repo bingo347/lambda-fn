9a3942c58144b5f71962311d06e43b57215a3861
Dmitry Belyaev <mail@d-belyaev.ru>
2021-09-10 17:21:51 +0300

    feat(basis): add SelectivePartial & SelectiveRequired utils

--- packages/basis/src/utils/transformTypes.ts
+++ packages/basis/src/utils/transformTypes.ts
@@ -8,3 +8,11 @@ export type Equal<A, B> = A extends B ? B extends A ? true : false : false;
 export type Recalculate<T> = T extends unknown ? {
     [K in keyof T]: T[K];
 } : never;
+
+export type SelectivePartial<T, K extends keyof T> =
+    & Required<Pick<T, Exclude<keyof T, K>>>
+    & Partial<Pick<T, K>>;
+
+export type SelectiveRequired<T, K extends keyof T> =
+    & Required<Pick<T, K>>
+    & Partial<Pick<T, Exclude<keyof T, K>>>;
