0fd200383062abe318c26039299a074f2de8f1f1
Dmitry Belyaev <mail@d-belyaev.ru>
2021-09-16 12:08:53 +0300

    fix(basis): asString return type support custom toString method

--- packages/basis/src/utils/strings.ts
+++ packages/basis/src/utils/strings.ts
@@ -3,7 +3,10 @@ import type {Strong} from './strongTypes';
 
 export type AsString<T, Default extends string = string> = T extends Exclude<Primitive, symbol> ? `${T}` : Default;
 
-export const asString = <T>(value: T): AsString<T> =>
-    String(value) as AsString<T>;
+type AsStringReturnType<T> = AsString<T, T extends {toString(): infer S}
+    ? Strong<S, string, string>
+    : string>;
+export const asString = <T>(value: T): AsStringReturnType<T> =>
+    String(value) as AsStringReturnType<T>;
 
 export type StrongString<Input> = Strong<Input, string, AsString<Input, ''>>;
