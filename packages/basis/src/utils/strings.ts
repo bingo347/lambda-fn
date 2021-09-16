2e0920f2128d76347b40d14860e02f1712964b7e
Dmitry Belyaev <mail@d-belyaev.ru>
2021-09-16 12:17:50 +0300

    fix(basis): AsString type suport custom toString too

--- packages/basis/src/utils/strings.ts
+++ packages/basis/src/utils/strings.ts
@@ -1,12 +1,13 @@
 import type {Primitive} from './primitive';
 import type {Strong} from './strongTypes';
 
-export type AsString<T, Default extends string = string> = T extends Exclude<Primitive, symbol> ? `${T}` : Default;
+export type AsString<T, Default extends string = string> = T extends Exclude<Primitive, symbol>
+    ? `${T}`
+    : T extends {toString(): infer S}
+        ? Strong<S, string, Default>
+        : Default;
 
-type AsStringReturnType<T> = AsString<T, T extends {toString(): infer S}
-    ? Strong<S, string, string>
-    : string>;
-export const asString = <T>(value: T): AsStringReturnType<T> =>
-    String(value) as AsStringReturnType<T>;
+export const asString = <T>(value: T): AsString<T> =>
+    String(value) as AsString<T>;
 
 export type StrongString<Input> = Strong<Input, string, AsString<Input, ''>>;
