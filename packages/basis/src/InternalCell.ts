02d1eab5ee99ec795db4b293340654a2359f0148
Dmitry Belyaev <mail@d-belyaev.ru>
2021-09-27 08:34:43 +0300

    fix: mark internal entities as ignore

--- packages/basis/src/InternalCell.ts
+++ packages/basis/src/InternalCell.ts
@@ -2,6 +2,7 @@ import {internal} from './internal';
 
 const InternalCellValues = new WeakMap<InternalCell<unknown>, unknown>();
 
+/** @ignore */
 export abstract class InternalCell<T> {
     constructor(value: T) {
         InternalCellValues.set(this, value);
--- packages/basis/src/internal.ts
+++ packages/basis/src/internal.ts
@@ -1,4 +1,4 @@
-/** @private */
+/** @ignore */
 export namespace internal { // eslint-disable-line @typescript-eslint/no-namespace
     export const InternalCellUnsafeValue = Symbol();
     export const InternalCellContextualFactory = Symbol();
