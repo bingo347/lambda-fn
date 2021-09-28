ed6d86fa791d92088333d51efb985fc666bb3d07
Dmitry Belyaev <mail@d-belyaev.ru>
2021-09-28 08:58:20 +0300

    fix(basis): categories is just interfaces

--- packages/basis/src/categories.ts
+++ packages/basis/src/categories.ts
@@ -1,38 +1,18 @@
-import {InternalCell} from './InternalCell';
-import {internal} from './internal';
-import type {Constructor} from './utils/constructors';
-import {create} from './utils/constructors';
 import type {Fn} from './utils/functions';
 
-export abstract class Functor<T> extends InternalCell<T> {
-    public map<R>(f: Fn<R, [value: T]>): Functor<R> {
-        const value = f(this[internal.InternalCellUnsafeValue]);
-        return this[internal.InternalCellContextualFactory](value);
-    }
-
-    public apply<V, R>(this: Functor<Fn<R, [value: V]>>, target: Functor<V>): Functor<R> {
-        return target.map(this[internal.InternalCellUnsafeValue]);
-    }
-
-    protected abstract override [internal.InternalCellContextualFactory]<V>(value: V): Functor<V>;
+export interface Functor<T> {
+    map<R>(f: Fn<R, [value: T]>): Functor<R>;
 }
 
-export abstract class Monad<T> extends Functor<T> {
-    public static of<V, M extends Monad<V>>(this: Constructor<M, [value: V]>, value: V): M {
-        return create(this, value);
-    }
+export interface ApplicativeFunctor<T> extends Functor<T> {
+    apply<V, R>(this: Functor<Fn<R, [value: V]>>, target: Functor<V>): Functor<R>;
+}
 
-    public unwrap(): T {
-        return this[internal.InternalCellUnsafeValue];
-    }
+export interface Monad<T> extends ApplicativeFunctor<T> {
+    unwrap(): T;
+    andThen<R>(f: Fn<Monad<R>, [value: T]>): Monad<R>;
+}
 
-    public andThen<R>(f: Fn<Monad<R>, [value: T]>): Monad<R> {
-        return this.map(f).unwrap();
-    }
-
-    public abstract override map<R>(f: Fn<R, [value: T]>): Monad<R>;
-
-    public abstract override apply<V, R>(this: Monad<Fn<R, [value: V]>>, target: Functor<V>): Monad<R>;
-
-    protected abstract override [internal.InternalCellContextualFactory]<V>(value: V): Monad<V>;
+export interface MonadOf {
+    of<V>(this: void, value: V): Monad<V>;
 }
