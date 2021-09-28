14863dfadd69c2041611b8a102a13539f3318006
Dmitry Belyaev <mail@d-belyaev.ru>
2021-09-28 08:59:25 +0300

    fix(basis): update internals

--- packages/basis/src/internal.ts
+++ packages/basis/src/internal.ts
@@ -1,5 +1,29 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+import {
+    isUndefined,
+    isBoolean,
+    isString,
+    isNumber,
+    isBigInt,
+    isSymbol,
+    isObject,
+    isFunction,
+} from './utils/guards';
+
 /** @ignore */
-export namespace internal { // eslint-disable-line @typescript-eslint/no-namespace
-    export const InternalCellUnsafeValue = Symbol();
-    export const InternalCellContextualFactory = Symbol();
+export namespace internal {
+    export const KIND = Symbol();
+}
+
+/** @ignore */
+export namespace guards {
+    export const u = isUndefined;
+    export const b = isBoolean;
+    export const s = isString;
+    export const n = isNumber;
+    export const bi = isBigInt;
+    export const sy = isSymbol;
+    export const o = isObject;
+    export const f = isFunction;
 }
