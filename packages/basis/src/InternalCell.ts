6116a0581866277e8292544a97eafe25545fa4e1
Dmitry Belyaev <mail@d-belyaev.ru>
2021-09-28 09:00:08 +0300

    fix(basis): simplify InternalCell

--- packages/basis/src/InternalCell.ts
+++ packages/basis/src/InternalCell.ts
@@ -1,16 +1,12 @@
-import {internal} from './internal';
-
 const InternalCellValues = new WeakMap<InternalCell<unknown>, unknown>();
 
 /** @ignore */
-export abstract class InternalCell<T> {
+export class InternalCell<T> {
     constructor(value: T) {
         InternalCellValues.set(this, value);
     }
 
-    protected get [internal.InternalCellUnsafeValue](): T {
+    public get unsafeValue(): T {
         return InternalCellValues.get(this) as T;
     }
-
-    protected abstract [internal.InternalCellContextualFactory]<V>(value: V): InternalCell<V>;
 }
