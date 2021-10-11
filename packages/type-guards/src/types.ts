217649a823b593a214a3d0e2979f7e2a2d48a1cd
Dmitry Belyaev <mail@d-belyaev.ru>
2021-10-12 01:00:07 +0300

    feat(type-guards): extend TypeGuard type

--- packages/type-guards/src/types.ts
+++ packages/type-guards/src/types.ts
@@ -1,4 +1,4 @@
-export type TypeGuard<T extends A, A = unknown> = (v: A) => v is T;
+export type TypeGuard<T extends V, V = unknown, Args extends unknown[] = unknown[]> = (v: V, ...args: Args) => v is T;
 
 export type AnyFunction = (...args: any[]) => unknown;
 export type AnyConstructor = new (...args: any[]) => unknown;
