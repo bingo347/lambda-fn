b3eea000426fe174830153e964572bf5c11f476a
Dmitry Belyaev <mail@d-belyaev.ru>
2021-10-12 01:36:26 +0300

    fix(type-guards): make TypeGuard type backwards compatible

--- packages/type-guards/src/types.ts
+++ packages/type-guards/src/types.ts
@@ -1,1 +1,1 @@
-export type TypeGuard<T extends V, V = unknown, Args extends unknown[] = unknown[]> = (v: V, ...args: Args) => v is T;
+export type TypeGuard<T extends V, V = unknown, Args extends unknown[] = []> = (v: V, ...args: Args) => v is T;
