17a1299694bed46128bd2ddf6c7a0badf0cab263
Dmitry Belyaev <mail@d-belyaev.ru>
2021-10-12 01:19:19 +0300

    fix(type-guards): reuse guards from basis

--- packages/type-guards/src/typeof.test.ts
+++ packages/type-guards/src/typeof.test.ts
@@ -1,17 +1,8 @@
-import type {TypeofResult} from './typeof';
 import {
     isString,
     isSymbol,
     isUndefined,
 } from './typeof';
-
-declare const __unknownValue: unknown;
-__checkAllTypesUsed();
-
-// it reports type mismatch if typeof will be extended in future standards
-function __checkAllTypesUsed(): TypeofResult {
-    return typeof __unknownValue;
-}
 
 test('isString', () => {
     expect(isString('')).toBe(true);
--- packages/type-guards/src/typeof.ts
+++ packages/type-guards/src/typeof.ts
@@ -1,36 +1,16 @@
 import type {AnyConstructor, AnyFn, Primitive} from '@lambda-fn/basis';
+import {guards} from '@lambda-fn/basis';
 import {isNull} from './values';
 
-type TypesMap = {
-    'undefined': undefined;
-    'boolean':   boolean;
-    'string':    string;
-    'number':    number;
-    'bigint':    bigint;
-    'symbol':    symbol;
-    'object':    Record<PropertyKey, unknown>;
-    'function':  AnyFn<unknown>;
-};
-
-export type TypeofResult = keyof TypesMap;
-
-const makeTypeofGuard = <T extends TypeofResult>(type: T) =>
-    (v: unknown): v is TypesMap[T] =>
-        // eslint-disable-next-line valid-typeof
-        typeof v === type;
-
-const isObjectRaw = /*@__PURE__*/makeTypeofGuard('object');
-
-export const isObject = (v =>
-    !isNull(v) && isObjectRaw(v)) as typeof isObjectRaw;
-
-export const isFunction = /*@__PURE__*/makeTypeofGuard('function');
-export const isSymbol = /*@__PURE__*/makeTypeofGuard('symbol');
-export const isString = /*@__PURE__*/makeTypeofGuard('string');
-export const isNumber = /*@__PURE__*/makeTypeofGuard('number');
-export const isBigInt = /*@__PURE__*/makeTypeofGuard('bigint');
-export const isBoolean = /*@__PURE__*/makeTypeofGuard('boolean');
-export const isUndefined = /*@__PURE__*/makeTypeofGuard('undefined');
+export const isObject = (v: unknown): v is Record<PropertyKey, unknown> =>
+    !isNull(v) && guards.o(v);
+export const isFunction: (v: unknown) => v is AnyFn<unknown> = guards.f;
+export const isUndefined = guards.u;
+export const isBoolean = guards.b;
+export const isString = guards.s;
+export const isNumber = guards.n;
+export const isBigInt = guards.bi;
+export const isSymbol = guards.sy;
 
 export const isPropertyKey = (v: unknown): v is PropertyKey =>
     isString(v) || isNumber(v) || isSymbol(v);
@@ -44,7 +24,7 @@ export const isNonNullable = <T>(v: T): v is NonNullable<T> =>
 export const isPrimitive = (v: unknown): v is Primitive =>
     isNullable(v) || isPropertyKey(v) || isBoolean(v) || isBigInt(v);
 
-export const isObjectLike = (v: unknown): v is TypesMap['object' | 'function'] =>
+export const isObjectLike = (v: unknown): v is AnyFn<unknown> | Record<PropertyKey, unknown> =>
     isObject(v) || isFunction(v);
 
 export const isConstructor = (v: unknown): v is AnyConstructor<unknown> & AnyFn<unknown> =>
