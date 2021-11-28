0f368cb890fad5a53ca7e287e4a4232caf405c3d
Dmitry Belyaev <mail@d-belyaev.ru>
2021-11-28 08:15:23 +0300

    feat(basis): add StrongBigint type

--- packages/basis/src/utils/numbers.ts
+++ packages/basis/src/utils/numbers.ts
@@ -1,3 +1,11 @@
 import type {Strong} from './strongTypes';
 
 export type StrongNumber<Input> = Strong<Input, number, 0>;
+
+/**
+ * Checks input type is bigint
+ * @returns input type if it is bigint or 0n otherwise
+ */
+// eslint-disable-next-line eslint-comments/no-restricted-disable
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers -- bigint cannot be excluded
+export type StrongBigint<Input> = Strong<Input, bigint, 0n>;
